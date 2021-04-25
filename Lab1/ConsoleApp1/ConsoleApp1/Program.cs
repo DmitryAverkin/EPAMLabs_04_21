c878680fe82fa4017df5c6790bee31de3e96d68f
Сергей <mitya.averkin2015@yandex.ru>
2021-04-25 19:44:43 +0300

    Add array and calculator

--- Lab1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int x;
+            Console.WriteLine("Введите размер массива:");
+            x = int.Parse(Console.ReadLine());
+            int[] ar = new int[x];
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine("Введите индекс массива [{0}] ", i);
+                    ar[i] = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("Элементы массива:");
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine("ar[{0}] = {1}", i, ar[i]);
+                }
+
+                Console.WriteLine("Отсортированный массив по возрастанию:");
+
+                int a;
+                for (int i = 0; i < x - 1; i++)
+                {
+                    for (int y = i + 1; y < x; y++)
+                    {
+                        if (ar[i] > ar[y])
+                        {
+                            a = ar[i];
+                            ar[i] = ar[y];
+                            ar[y] = a;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine("ar[{0}] = {1}", i, ar[i]);
+                }
+            }
+        }     
     }
 }
