d2950be1369a98cc3858ef05b87ddb87b792fb44
Сергей <mitya.averkin2015@yandex.ru>
2021-05-09 19:33:23 +0300

    Create Lab5

---
+++ Lab1/Lab5/Lab5/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class Rectangle
+    {
+        [Test, TestCaseSource("Dataset")]
+        public void TestArea(double x, double y)
+        {
+            var area = GeometricFigures.Rectangle.GetArea(x, y);
+            Assert.AreEqual(x * y, area, $"Ожидаем {x * y}, а в итоге получаем {area}");
+        }
+
+        [Test, TestCaseSource("Dataset")]
+        public void TestPerimeter(double x, double y)
+        {
+            var perimeter = GeometricFigures.Rectangle.GetLength(x, y);
+            Assert.AreEqual(2 * (x + y), perimeter, $"Ожидаем {2 * (x + y)}, а в итоге получаем {perimeter}");
+        }
+
+        static object[] Dataset =
+        {
+            new object[] {1,2},
+            new object[] {3,7},
+            new object[] {4,1},
+            new object[] {15,12},
+            new object[] {14,14},
+            new object[] {10,15},
+            new object[] {7,8},
+            new object[] {18,19},
+            new object[] {4,17},
+            new object[] {19,18},
+            new object[] {10,10},
+            new object[] {12,7},
+            new object[] {16,2},
+            new object[] {1,18},
+            new object[] {9,5},
+            new object[] {15,14},
+            new object[] {19,2},
+            new object[] {5,6},
+            new object[] {4,11},
+            new object[] {7,1},
+            new object[] {19,4},
+        };
+    } 
+}
---
+++ Lab1/Lab5/Lab5/Square.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class Square
+    {
+        [Test, TestCaseSource("Dataset")]
+        public void TestArea(double x)
+        {
+            var area = GeometricFigures.Square.GetArea(x);
+            Assert.AreEqual(x * x, area, $"Ожидаем {x * x}, а в итоге получаем {area}");
+        }
+
+        [Test, TestCaseSource("Dataset")]
+        public void TestPerimeter(double y)
+        {
+            var perimeter = GeometricFigures.Square.GetLength(y);
+            Assert.AreEqual(y * 4, perimeter, $"Ожидаем {y * 4}, а в итоге получаем {perimeter}");
+        }
+
+        static object[] Dataset =
+        {
+            new object[] {0},
+            new object[] {1},
+            new object[] {2},
+            new object[] {3},
+            new object[] {4},
+            new object[] {5},
+            new object[] {6},
+            new object[] {7},
+            new object[] {8},
+            new object[] {9},
+            new object[] {10},
+            new object[] {11},
+            new object[] {12},
+            new object[] {13},
+            new object[] {14},
+            new object[] {15},
+            new object[] {16},
+            new object[] {17},
+            new object[] {18},
+            new object[] {19},
+            new object[] {20},
+        };
+    }
+}
